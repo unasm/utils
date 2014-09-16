6872cc2e728ac81426ef321d4775767071096c23
tianyi <douunasm@gmail.com>
2014-09-16 17:38:54 +0800

    完成了红黑树，基本，还需要进行大数据测试

--- rbtree.cpp
+++ rbtree.cpp
@@ -5,80 +5,81 @@
 using namespace   std;
 int deep(int pos ,int blackDeepth);
 #define LENGTH 20000
-//ÕâÀï¶¨ÒåµÄºìºÚÊ÷ÊÇ×ó±ßÐ¡£¬ÓÒ±ß´ó
+#define TEST 10
+//?a¨¤??¡§¨°?¦Ì?o¨¬o¨²¨º¡Â¨º?¡Á¨®¡À?D?¡ê?¨®¨°¡À?¡ä¨®
 struct treeNode{
     unsigned int hashValue;
-    //@todo ÕâÀïÔÝÊ±Ê¹ÓÃÊý×é´úÌæ£¬½«À´¸ù¾Ý¾ßÌåµÄÊý¾Ý£¬¿¼ÂÇÊ¹ÓÃÁ´±íÓÅ»¯
-    //±£´æhttpÁ¬½ÓµÄ×îºóÊ®¸ö×Ö·û£¬×÷ÎªÅÐÖØµÄ±ê×¼
+    //@todo ?a¨¤??Y¨º¡À¨º1¨®?¨ºy¡Á¨¦¡ä¨²¨¬?¡ê???¨¤¡ä?¨´?Y??¨¬?¦Ì?¨ºy?Y¡ê?????¨º1¨®?¨¢¡ä¡À¨ª¨®??¡¥
+    //¡À¡ê¡ä?http¨¢??¨®¦Ì?¡Á?o¨®¨º???¡Á?¡¤?¡ê?¡Á¡Â?a?D??¦Ì?¡À¨º¡Á?
     char httpBack[3][10];
-    char color;// -1±íÊ¾nil£¬0±íÊ¾ºì£¬1±íÊ¾ºÚ
+    char color;// -1¡À¨ª¨º?nil¡ê?0¡À¨ª¨º?o¨¬¡ê?1¡À¨ª¨º?o¨²
     int right,left, parent;
 }list[LENGTH];
 int queue[LENGTH][2];
-//±íÊ¾list½«ÒªÊ¹ÓÃµÄÏÂ±í
+//¡À¨ª¨º?list??¨°a¨º1¨®?¦Ì???¡À¨ª
 int listPos = 0,ROOT;
-//@todo ÒÔºóÔÙ¿¼ÂÇ×·¼ÓÊý×é
+//@todo ¨°?o¨®?¨´????¡Á¡¤?¨®¨ºy¡Á¨¦
 /**
- * time33Ëã·¨£¬½«×Ö·û´®×ª»¯³ÉÎª¶ÔÓ¦µÄint
- * @param	char *hash ÐèÒª×ª»¯µÄ×Ö·û´®
+ * time33??¡¤¡§¡ê???¡Á?¡¤?¡ä?¡Áa?¡¥3¨¦?a??¨®|¦Ì?int
+ * @param	char *hash D¨¨¨°a¡Áa?¡¥¦Ì?¡Á?¡¤?¡ä?
  */
-unsigned int time33(char *hash){
+unsigned int time33(string url){
     unsigned int ret = 0;
-    while(*hash !='\0'){
-        ret =( ret << 5) + ret + (*hash++);
-    }
-    ret &=(1 << 31) - 1;
+    for(std::string::iterator it = url.begin();it != url.end();it++){
+		ret +=( ret << 5) + (*it);
+	}
+	ret &=(1<<31) - 1;
     return ret;
 }
 /**
- * ×óÐý
- * @param	int		pos		¹Ø½Ú½Úµã
+ * ¡Á¨®Dy
+ * @param	int		pos		1??¨²?¨²¦Ì?
  */
 void rotateLeft(int parent){
 	int grandpa = list[parent].parent;
 	int n = list[parent].right;
-	//nºÍg Ïà»¥Á¬½Ó
+	//no¨ªg ?¨¤?£¤¨¢??¨®
 	if(grandpa != -1){
 		list[grandpa].left = n;
 	}
 	list[n].parent = grandpa;
-	//pºÍnµÄ×ó½Úµã»¥Ïà½»»»
+	//po¨ªn¦Ì?¡Á¨®?¨²¦Ì??£¤?¨¤????
 	if(list[n].left!= -1){
 		list[list[n].left].parent = parent;
 	}
 	list[parent].right = list[n].left;
-	//nºÍp»¥Ïà½»»»
+	//no¨ªp?£¤?¨¤????
 	list[parent].parent = n;
 	list[n].left = parent;
 }
 /**
- * ÓÒÐý
+ * ¨®¨°Dy
  *@param int parent
  */
 void rotateRight(int parent){
 	int grandpa = list[parent].parent;
 	int n = list[parent].left;
-	//n½ÚµãºÍ×æ¸¸½Úµã»¥Ïà½»»»
+	//n?¨²¦Ì?o¨ª¡Á????¨²¦Ì??£¤?¨¤????
 	if(grandpa != -1){
 		list[grandpa].right = n;
 	}
 	list[n].parent= grandpa;
-	//nÓÒ½ÚµãºÍp½ÚµãµÄ»¥Ïà½»»»
+	//n¨®¨°?¨²¦Ì?o¨ªp?¨²¦Ì?¦Ì??£¤?¨¤????
 	list[parent].left = list[n].right ;
 	if(list[n].right != -1){
 		list[list[n].right].parent = parent;
 	}
-	//nºÍp»¥Ïà½»»»
+	//no¨ªp?£¤?¨¤????
 	list[n].right = parent;
 	list[parent].parent = n;
 }
 void rotate5(int pos){
 	int parent = list[pos].parent;
 	int grandpa = list[parent].parent;
-	//¸¸½Úµã±ä³ÉºÚÉ«£¬×æ¸¸½Úµã±à³ÌºìÉ«
+	//???¨²¦Ì?¡À?3¨¦o¨²¨¦?¡ê?¡Á????¨²¦Ì?¡À¨¤3¨¬o¨¬¨¦?
     list[parent].color = 1;
     list[grandpa].color = 0;
-	//¶¼ÊÇ×ó½ÚµãµÄÇé¿ö£¬ÓÒÐý×ª
+	//??¨º?¡Á¨®?¨²¦Ì?¦Ì??¨¦??¡ê?¨®¨°Dy¡Áa
     if(list[parent].left == pos && list[grandpa].left == parent){
         rotateRight(grandpa);
     }else{
@@ -86,86 +87,85 @@ void rotate5(int pos){
     }
 }
 /**
- *@param int  ½ÚµãÔÚÊ÷ÖÐµÄÎ»ÖÃ
+ *@param int  ?¨²¦Ì??¨²¨º¡Â?D¦Ì?????
  */
 void fixColor(int pos){
-    //µÚÒ»ÖÖÇé¿ö£¬¸¸Ç×½ÚµãÊÇºÚÉ«µÄ£¬²»ÓÃµ÷Õû
-    //Èç¹ûµ±Ç°½ÚµãÎª¸ù½ÚµãµÄÇé¿ö,µ±×æ¸¸½ÚµãÎª-1£¬Ôò¸¸½ÚµãÎª¸ú½Úµã£¬Ö±½Ó¸ü¸Ä¸ù½ÚµãÎªºÚÉ«£¬³åÍ»½â¾ö
-    int parent = list[pos].parent,grandpa = list[parent].parent;
-    if(pos == 0 ){
-		printf("ÕâÀï²»Ó¦¸ÃÓÐÊ²Ã´½ø\n");
+    //¦Ì¨²¨°????¨¦??¡ê????¡Á?¨²¦Ì?¨º?o¨²¨¦?¦Ì?¡ê?2?¨®?¦Ì¡Â??
+    //¨¨?1?¦Ì¡À?¡ã?¨²¦Ì??a?¨´?¨²¦Ì?¦Ì??¨¦??,¦Ì¡À¡Á????¨²¦Ì??a-1¡ê??¨°???¨²¦Ì??a?¨²?¨²¦Ì?¡ê??¡À?¨®?¨¹???¨´?¨²¦Ì??ao¨²¨¦?¡ê?3?¨ª??a??
+    int parent = list[pos].parent;
+    //the first situation
+    if(list[pos].parent == -1){
 		list[pos].color = 1;
+		return;
     }
-	if(parent == 0){
-		list[parent].color = -1;
-        return;
-	}
-	//µÚ¶þÇé¿ö£¬¸¸½ÚµãÎªºÚÉ«£¬²»³åÍ»
-	if(list[parent].color == 1)return;
-
-	int uncle ;
-	//²éÕÒÊåÊå½Úµã
+    // only when pos and parent is the red ,we fix;
+    if(list[pos].color != 0 || list[parent].color != 0){
+    	return ;
+    }
+   	int grandpa = list[parent].parent,uncle;
+   	//the second situation the grandpa is the root 
+	//¦Ì¨²?t?¨¦??¡ê????¨²¦Ì??ao¨²¨¦?¡ê?2?3?¨ª?
+	//get uncle
     if(list[grandpa].right == parent){
          uncle = list[grandpa].left;
     }else{
         uncle = list[grandpa].right;
     }
-    //ÊåÊå²»Îª¿Õ£¬½ÚµãµÄÑÕÉ«Îªºì
+    //¨º?¨º?2??a??¡ê??¨²¦Ì?¦Ì???¨¦??ao¨¬
     if(uncle != -1 && list[uncle].color == 0){
         list[uncle].color = 1;
         list[parent].color = 1;
         list[grandpa].color = 0;
         fixColor(grandpa);
+        return;
     }else if(parent == list[grandpa].right && pos == list[parent].left){
-        //ÊåÊå½ÚµãµÄÑÕÉ«ÎªºÚ»òÕßÊÇÒ¶×Ó½Úµã,¸¸Ç×ÎªÓÒ½Úµã£¬×Ô¼ºÎª×ó½Úµã
+        //¨º?¨º??¨²¦Ì?¦Ì???¨¦??ao¨²?¨°??¨º?¨°?¡Á¨®?¨²¦Ì?,???¡Á?a¨®¨°?¨²¦Ì?¡ê?¡Á??o?a¡Á¨®?¨²¦Ì?
         rotateRight(parent);
 		pos = list[pos].right;
-        //fixColor(parent);
+       
     }else if(parent == list[grandpa].left && pos == list[parent].right){
         rotateLeft(parent);
 		pos = list[pos].left;
     }
 	rotate5(pos);
+	fixColor(list[pos].parent);
 }
 
 /**
- * @param	int		hashValue	hash³öÀ´µÄÊýÖµ
- * @param	char *	url			ºó10Îª×Ö·û
- * @param	int		pos			½ÚµãµÄÎ»ÖÃ
+ * @param	int		hashValue	hash3?¨¤¡ä¦Ì?¨ºy?¦Ì
+ * @param	char *	url			o¨®10?a¡Á?¡¤?
+ * @param	int		pos			?¨²¦Ì?¦Ì?????
  */
-int rbtree(int hashValue , char *url, int pos){
-    //´¦Àí¸ùÄ¿Â¼ºÍÏàÍ¬µÄÇé¿ö
-	//½øÈëÒ¶×Ó½Úµã
+void rbtree(int hashValue , string url, int pos){
+    //¡ä|¨¤¨ª?¨´????o¨ª?¨¤¨ª?¦Ì??¨¦??
+	//??¨¨?¨°?¡Á¨®?¨²¦Ì?
     if(list[pos].color == -1){
         list[pos].hashValue = hashValue;
         list[pos].color = 0;
         int cnt = 0;
-        //²éÒ»ÏÂÈçºÎ½«×Ö·û´®¸³Öµ¸ø×Ö·û
-        while(*url !='\0'){
-            list[pos].httpBack[0][cnt++] = *url++;
-		//	printf("%s\n" , list[pos].httpBack[0]);
+        //2¨¦¨°???¨¨?o???¡Á?¡¤?¡ä??3?¦Ì??¡Á?¡¤?
+        for(string::iterator it = url.begin();it != url.end();it++){
+        	list[pos].httpBack[0][cnt++]= *it;
         }
-        //Èç¹û¸¸½ÚµãÒ²ÎªºìÉ«£¬ÔòÃ¬¶Ü·¢Éú
-        if(list[list[pos].parent].color == 0){
-            fixColor(pos);
-        }
-		return -1;
+        //¨¨?1????¨²¦Ì?¨°2?ao¨¬¨¦?¡ê??¨°???¨¹¡¤¡é¨¦¨²
+     fixColor(pos);
+		return ;
     }
-	//Èç¹û²»ÊÇÒ¶×Ó½Úµã£¬µ«ÊÇÊýÖµÏàÍ¬£¬´¦Àí³åÍ»
+	//¨¨?1?2?¨º?¨°?¡Á¨®?¨²¦Ì?¡ê?¦Ì?¨º?¨ºy?¦Ì?¨¤¨ª?¡ê?¡ä|¨¤¨ª3?¨ª?
 	if(list[pos].hashValue == hashValue){
-		printf("·¢ÉúÁË³åÍ»\n");
-		return -1;
+		printf("-1 the sameing is coming\n");
+		return;
 	}
 	if(hashValue > list[pos].hashValue){
-        //´óÊý×ÖÔÚÓÒ±ß
+        //¡ä¨®¨ºy¡Á??¨²¨®¨°¡À?
 		if(list[pos].right == -1){
-			//Èç¹û×ó½Úµã»òÕßÊÇÓÒ½ÚµãÊôÓÚÉÐÎ´·ÖÅäµÄ½Úµã£¬½«ÆäÓë¸¸¹ØÏµÉùÃ÷
+			//¨¨?1?¡Á¨®?¨²¦Ì??¨°??¨º?¨®¨°?¨²¦Ì?¨º?¨®¨²¨¦D?¡ä¡¤???¦Ì??¨²¦Ì?¡ê?????¨®???1??¦Ì¨¦¨´?¡Â
 			list[listPos].parent = pos;
 			list[pos].right = listPos++;
 		}
         rbtree(hashValue,url,list[pos].right);
     }else{
-        //Ð¡Êý×ÖÔÚ×ó±ß
+        //D?¨ºy¡Á??¨²¡Á¨®¡À?
 		if(list[pos].left == -1){
 			list[listPos].parent = pos;
 			list[pos].left = listPos++;
@@ -180,15 +180,15 @@ int findRoot(int node){
 	return node;
 }
 /**
- * µ±µ±Ç°½ÚµãÎªºìÉ«µÄÊ±ºò£¬¶Ô×Ó½Úµã½øÐÐµÄ¼ìÑé
+ * ¦Ì¡À¦Ì¡À?¡ã?¨²¦Ì??ao¨¬¨¦?¦Ì?¨º¡Ào¨°¡ê???¡Á¨®?¨²¦Ì???DD¦Ì??¨¬?¨¦
  */
 int redCheck(int pos ,int blackDeepth){
 	if(pos != -1 && list[pos].color != -1){
 		if(list[pos].color == 0)	{
-			printf("ÑÕÉ«²»¶Ô,¸¸½ÚµãºÍ×Ó½Úµã¶¼ÎªºìÉ«ÁË");
+			printf("??¨¦?2???,???¨²¦Ì?o¨ª¡Á¨®?¨²¦Ì????ao¨¬¨¦?¨¢?");
 			return -2;
 		}
-		//×Ó½ÚµãÎªÒ¶×Ó½Úµã£¬²»ÔÙ¼ÓÉî
+		//¡Á¨®?¨²¦Ì??a¨°?¡Á¨®?¨²¦Ì?¡ê?2??¨´?¨®¨¦?
 		if(list[pos].color == 1){
 			return deep(pos,blackDeepth);
 		}
@@ -196,13 +196,13 @@ int redCheck(int pos ,int blackDeepth){
 	return blackDeepth;
 }
 /**
- * µÝ¹é¼ìÑéÊÇ²»ÊÇºìºÚÊ÷
- * @param int pos			µ±Ç°½ÚµãµÄÎ»ÖÃ
- * @param int blackDeepth	¾­ÀúµÄºÚ½ÚµãµÄÊýÄ¿
+ * ¦ÌY1¨¦?¨¬?¨¦¨º?2?¨º?o¨¬o¨²¨º¡Â
+ * @param int pos			¦Ì¡À?¡ã?¨²¦Ì?¦Ì?????
+ * @param int blackDeepth	?-¨¤¨²¦Ì?o¨²?¨²¦Ì?¦Ì?¨ºy??
  */
 int deep(int pos ,int blackDeepth){
 	int rightValue = 0,leftValue = 0;
-	//ºÚÉ«µÄ£¬²»ÓÃ¼Æ½Ï£¬Ö±½Ó+1
+	//o¨²¨¦?¦Ì?¡ê?2?¨®?????¡ê??¡À?¨®+1
 	if(list[pos].color == 1){
 		if(list[pos].right != -1){
 			rightValue = deep(list[pos].right , blackDeepth + 1);
@@ -216,7 +216,7 @@ int deep(int pos ,int blackDeepth){
 		leftValue = redCheck(list[pos].right , blackDeepth);
 	}
 	if(list[pos].color == -1){
-		printf("³öÏÖÁË²»Ó¦¸Ã³öÏÖµÄÇé¿ö£¬-1²»Ó¦¸Ã³öÏÖÔÚÕâÀï");
+		printf("-1 is coming back");
 		return -2;
 	}
 	if(rightValue && leftValue ){
@@ -225,18 +225,17 @@ int deep(int pos ,int blackDeepth){
 	return rightValue | leftValue;
 }
 /**
- * ¼ìÑéÊ÷ÊÇ²»ÊÇºìºÚÊ÷
+ * ?¨¬?¨¦¨º¡Â¨º?2?¨º?o¨¬o¨²¨º¡Â
  */
 int checkIsRBtree(int root ){
 	int in = 0,out = 0;
 	if(list[root].color != 1){
-		printf("¸ù½ÚµãÃ»ÓÐÎªºÚÉ«");
+		printf("");
 		return -2;
 	}
 	int value = deep(root ,1 );
 	if(value > 0){
 		;
-		//printf("ÊÇºìºÚÊ÷");
 	}else{
 		printf("error : %d\n", value);
 	}
@@ -244,33 +243,54 @@ int checkIsRBtree(int root ){
 }
 int  main(){
 	int i ,j;
-    string urls[2][10] = {
-        {"sdf" , "sdfsadf" , "sd"},    {"sdf" , "sdfsadf" , "sd"}
+    string urls[TEST][20] = {
+		{"sbbf" , "bbb" , "sbb","s" , "sdf" , "dsfa" , "sdfad"},
+		{"sbbsf" , "bbb" , "sbb","s" , "sdf" , "dsfa" , "sdfad"},
+		{"sbbsf" ,"sbb","s" , "sdf" , "sdfad"},
+		{"sbbsf" , "bbb" , "sbb","s" , "sdf" , "dsfa" , "sdfad","sbbsf" , "bbb" , "sbb","s" , "sdf" , "dsfa" , "sdfad"}
     };
-    for(i = 0;i < LENGTH;i++){
-        list[i].color =  -1;
-        list[i].right =  -1;
-        list[i].left =  -1;
-    }
-	//Ô¤´¦Àí¸ù½ÚµãÎª¿ÕµÄÇé¿ö
-	listPos = 0;
-	list[listPos].color = 1;
-	//list[listPos].hashValue = time33(urls[0]);
-	list[listPos].parent = -1;
-	listPos++;
-    /*
-	for(j=0;j < 1;j++){
-	    for(i = 1;i < 3;i++){
-    	    unsigned int tmp = time33(urls[j][i].c_str());
-	       // printf("%d\n" , tmp);
-        	rbtree(tmp,urls[j][i].c_str(),findRoot(0));
-    	}
+	for(j=0;j < TEST && urls[j][0] != "";j++){
+ 		for(i = 0;i < LENGTH;i++){
+        	list[i].color =  -1;
+	        list[i].right =  -1;
+    	    list[i].left =  -1;
+	    }
+		listPos = 0;
+		list[listPos].color = 1;
+  		cout << time33(urls[j][0]) << endl;
+		list[listPos].hashValue = time33(urls[j][0]);
+		list[listPos].parent = -1;
+		listPos++;
+	    for(i = 1;urls[j][i] != "";i++){ 
+		    ROOT = findRoot(0);
+		    cout << time33(urls[j][i]) << endl;
+        	rbtree(time33(urls[j][i]),urls[j][i],ROOT);
+   			if(checkIsRBtree(findRoot(0)) == -2){
+				printf("return value is -2\n");
+				if(getchar() == '\n'){
+					;
+				}
+   			}
+ 		}
+    	for(i=0;i < listPos;i ++){
+	    	printf("%d \thashValue = %d , left is %d , right is %d  , parent is %d ",i,list[i].hashValue,list[i].left , list[i].right ,list[i].parent);
+	    	if(list[i].color == 0){
+				printf("red");
+			} else if(list[i].color == 1){
+				printf("black");
+			} else{
+				printf("sec:%d" , list[i].color);
+			}
+			printf("\n");
+	    }
+   		printf("root : %d\n" , findRoot(0));
+		if(checkIsRBtree(findRoot(0)) == -2){
+			printf("return value is -2\n");
+		}
+		if(getchar()== '\n'){
+			;
+		}
    	}
-    */
-	printf("root : %d\n" , findRoot(0));
-	if(checkIsRBtree(findRoot(0)) == -2){
-		printf("²»ÊÇºìºÚÊ÷");
-	}
-    //cout << time33(hash)<< endl;
+
     return 0;
 }
