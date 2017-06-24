56c0116fe3ff38cf69c01076ceb05fbe368ba28c
豆家敏 <doujm@jiedaibao.com>
2017-06-24 18:58:49 +0800

    添加json To Csv

---
+++ JsontoCsv.py
@@ -0,0 +1,43 @@
+# -*- coding: UTF-8 -*-
+#
+# File Name    :    JsontoCsv.py
+# Author       :    doujm
+# Mail         :    doujm@jiedaibao.com
+# Create Time  :    2017-06-24 17:27:56
+############################################### 
+
+import os
+import sys
+import json
+
+fd = open(sys.argv[1])
+data =  fd.readline()
+firstLine = json.loads(data)
+template = {}
+idxPos = 0
+for key in firstLine:
+    template[key] = idxPos
+    idxPos += 1
+#print template
+
+linePos = 1
+lenTemplate = len(template)
+resStr = "\t" . join(firstLine.values()) + "\n"
+try: 
+    for line in fd:
+        #print line
+        row = json.loads(line)
+        if len(row) != lenTemplate:
+            raise ValueError("数值不统一 line : %s , value : %s" % (linePos, line))
+
+        for key in row:
+            if not template.has_key(key):
+                raise ValueError("异常key : %s , line : %s, value : %s" % (key, linePos, line))
+
+        resStr += "\t" . join(row.values()) + "\n"
+        linePos += 1
+    #print resStr
+    print resStr
+finally:
+    fd.close()
+
